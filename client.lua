2892a4938903c6b2ababcfb46002fa9c83391a45
Arda03 <146008150+Arda3432@users.noreply.github.com>
2025-08-24 03:54:25 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,18 +1,1 @@
-CreateThread(function()
-    while true do
-        SetVehicleDensityMultiplierThisFrame(0.0)
-        SetPedDensityMultiplierThisFrame(0.0)
-        SetRandomVehicleDensityMultiplierThisFrame(0.0)
-        SetParkedVehicleDensityMultiplierThisFrame(0.0)
-        SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0)
 
-        ClearAreaOfPeds(GetEntityCoords(PlayerPedId()), 1000.0, 1)
-        ClearAreaOfVehicles(GetEntityCoords(PlayerPedId()), 1000.0, false, false, false, false, false)
-
-        SetCreateRandomCops(false)
-        SetCreateRandomCopsNotOnScenarios(false)
-        SetCreateRandomCopsOnScenarios(false)
-
-        Wait(0)
-    end
-end)
